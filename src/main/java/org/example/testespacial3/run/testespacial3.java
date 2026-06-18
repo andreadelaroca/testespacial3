92d513dcc5ef513557a3653474b90410c03c5b54
andreadelaroca <asodelaroca@uamv.edu.ni>
2026-06-18 13:46:45 -0600

    feat: Configuración de PostgreSQL

---
+++ src/main/java/org/example/testespacial3/run/testespacial3.java
@@ -0,0 +1,16 @@
+package org.example.testespacial3.run;
+
+import org.openxava.util.*;
+
+/**
+ * Ejecuta esta clase para arrancar la aplicación.
+ */
+
+public class testespacial3 {
+
+	public static void main(String[] args) throws Exception {
+		DBServer.start("testespacial3-db"); // Para usar tu propia base de datos comenta esta línea y configura src/main/webapp/META-INF/context.xml
+		AppServer.run("testespacial3"); // Usa AppServer.run("") para funcionar en el contexto raíz
+	}
+
+}
