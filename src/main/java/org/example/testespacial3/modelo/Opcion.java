3b4fc26b4d1dccc363316352ee2225ea06c4ad8b
andreadelaroca <asodelaroca@uamv.edu.ni>
2026-06-20 11:46:40 -0600

    fix: Dependencias javax

--- src/main/java/org/example/testespacial3/modelo/Opcion.java
+++ src/main/java/org/example/testespacial3/modelo/Opcion.java
@@ -1,16 +1,14 @@
-import javax.persistence.Entity;
+import org.example.testespacial3.modelo;
+import javax.persistence.*;
+import org.openxava.annotations.*;
+import lombok.*;
 
-@Entity
+@Entity @Getter @Setter
 public class Opcion {
 
-	@Required
+	@Required @Column(length = 5)
 	private boolean acierto;
 
-
+	@Required @Column(length = 2)
 	private int respuesta;
-
-	public Opcion(){
-
-	}
-
 }
