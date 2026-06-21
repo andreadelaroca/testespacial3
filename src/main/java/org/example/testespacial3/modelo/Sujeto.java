064e7827f01b24d7e97c1fab7ec366cba6d73743
andreadelaroca <asodelaroca@uamv.edu.ni>
2026-06-20 19:37:01 -0600

    feat: clase usuario

--- src/main/java/org/example/testespacial3/modelo/Sujeto.java
+++ src/main/java/org/example/testespacial3/modelo/Sujeto.java
@@ -1,3 +1,10 @@
+package org.example.testespacial3.modelo;
+
+import javax.persistence.*;
+import lombok.*;
+import org.openxava.
+
+@Entity @Getter @Setter
 public class Sujeto extends Usuario {
 
 	public Sujeto(){
--- src/main/java/org/example/testespacial3/modelo/Usuario.java
+++ src/main/java/org/example/testespacial3/modelo/Usuario.java
@@ -1,33 +1,66 @@
 package org.example.testespacial3.modelo;
 
 import lombok.*;
+
+import javax.ejb.Local;
 import javax.persistence.*;
 import org.openxava.annotations.*;
-import java.util.Date;
+import java.time.*;
+import org.hibernate.annotations.Where;
 
 @Entity @Getter @Setter @Table(name = "usuario")
 public class Usuario {
 
-	@Id @Hidden @GeneratedValue
+	@Id @Hidden @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(name = "usuarioid")
 	private int id;
 
-	private String nombres;
+	@Required @Column(name = "username", length = 255)
+	private String username;
 
-	private String apellidos;
-
-	private String email;
-
-	private String estudios;
-
-	private Date fechaNac;
-
+	@Required @Column(name = "password", length = 255) @Stereotype("PASSWORD")
 	private String password;
 
-	private String profesion;
+	@Required @Column(name = "nombres", length = 255)
+	private String nombres;
 
+	@Required @Column(name = "apellidos", length = 255)
+	private String apellidos;
+
+	@Column(name = "email", length = 255) @Stereotype("EMAIL")
+	private String email;
+
+	@Column(name = "fechanac")
+	private LocalDate fechaNac;
+
+	@Column(name = "sexo", length = 1)
 	private char sexo;
 
-	private String username;
+	@Column(name = "estudios", length = 255)
+	private String estudios;
+
+	@Column(name = "profesion", length = 255)
+	private String profesion;
+
+	//tiempos para fines de auditoria
+	@ReadOnly @Column(name = "createdat", updatable = false)
+	private LocalDateTime createdAt;
+
+	@ReadOnly @Column(name = "updatedat")
+	private LocalDateTime updatedAt;
+
+	@Hidden @Column(name = "deletedat")
+	private LocalDateTime deletedAt;
+
+	@PrePersist
+	protected void onCreate() {
+		this.createdAt = LocalDateTime.now();
+		this.updatedAt = LocalDateTime.now();
+	}
+	@PreUpdate
+	protected void onUpdate() {
+		this.updatedAt = LocalDateTime.now();
+	}
+
 	public void cerrarSesion(){
 
 	}
