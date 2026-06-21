1e73ef52dad606b262096322d50cb0ed6d012685
andreadelaroca <asodelaroca@uamv.edu.ni>
2026-06-20 19:39:11 -0600

    feat: clase sujeto

--- src/main/java/org/example/testespacial3/modelo/Sujeto.java
+++ src/main/java/org/example/testespacial3/modelo/Sujeto.java
@@ -2,14 +2,14 @@ package org.example.testespacial3.modelo;
 
 import javax.persistence.*;
 import lombok.*;
-import org.openxava.
+import org.openxava.annotations.*;
+import java.util.*;
 
-@Entity @Getter @Setter
+@Entity @Getter @Setter @Table(name = "sujeto") @PrimaryKeyJoinColumn(name = "sujetoid")
 public class Sujeto extends Usuario {
 
-	public Sujeto(){
-
-	}
+	@OneToMany(mappedBy = "sujeto", cascade = CascadeType.ALL) @ReadOnly @ListProperties("fecha, horaInicio, test.nombre, aciertos, desaciertos")
+	private Collection<DetalleAplicacion> aplicaciones;
 
 	public void realizarTest(){
 
