f7018cc2cd0a459313f6da12a9623b5c449c49ff
andreadelaroca <asodelaroca@uamv.edu.ni>
2026-06-24 02:23:40 -0600

    feat: inicio de sesión

---
+++ src/main/java/org/example/testespacial3/actions/IniciarSesion.java
@@ -0,0 +1,35 @@
+package org.example.testespacial3.actions;
+
+import org.openxava.actions.*;
+import org.openxava.jpa.*;
+import javax.persistence.*;
+
+public class IniciarSesion extends ViewBaseAction {
+    public void execute() throws Exception {
+        String username = getView().getValueString("usuario");
+        String password = getView().getValueString("contrasena");
+
+        try {
+            EntityManager em = XPersistence.getManager();
+            Query query = em.createQuery("SELECT u FROM usuario u WHERE u.username = :username AND u.password = :password");
+            query.setParameter("username", username);
+            query.setParameter("password", password);
+
+            Object usuarioLogueado = query.getSingleResult();
+
+            if (usuarioLogueado.getClass().getSimpleName().contains("Sujeto")) {
+                getRequest().getSession().setAttribute("xava.user", username);
+                String urlTest = getRequest().getContextPath() + "/test-sujeto.html";
+                getView().setNextURL(urlTest);
+
+            } else {
+                getRequest().getSession().setAttribute("xava.user", username);
+                String urlAdmin = getRequest().getContextPath() + "/m/Test";
+                getView().setNextURL(urlAdmin);
+            }
+
+        } catch (NoResultException e) {
+            addError("usuario_o_password_incorrecto");
+        }
+    }
+}
